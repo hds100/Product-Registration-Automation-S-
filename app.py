42acf67b694bae6cf5db33e5a0922a734adb0179
João Pedro H <148555673+hds100@users.noreply.github.com>
2023-11-06 22:49:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-import pyautogui
-from time import sleep
+import pyautogui # Importing pyautogui
+from time import sleep # Importing the sleep method from the time library
 
-with open('produtos.txt','r') as file:
+# Opening the Produtos.txt text file and granting read-only permission to the registry variables
+with open('produtos.txt','r') as file: 
+    # Defining variables and reading order
     for line in file:
         product = line.split(',')[0]
         quantity = line.split(',')[1]
         price = line.split(',')[2]
-
+        
+    # Defining variable settings: click, time and writing
+    # Click: cursor position coordinates through mouseinfo()
+    # Time: set to 100 milliseconds = 0.1 seconds interval between actions to be performed
         pyautogui.click(121,259, duration=0.1)
         pyautogui.write(product)
 
